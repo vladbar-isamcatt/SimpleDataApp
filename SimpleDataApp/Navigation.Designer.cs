b88df491aca9d0315c1afe46b585ccc183e71545
Владислав Барабонов <mail.vladbar@gmail.com>
2021-04-14 13:13:20 +0400

    Добавление кода для логики формы Navigation

--- SimpleDataApp/Navigation.Designer.cs
+++ SimpleDataApp/Navigation.Designer.cs
@@ -38,7 +38,7 @@ namespace SimpleDataApp
 			// labelSelectAct
 			// 
 			this.labelSelectAct.AutoSize = true;
-			this.labelSelectAct.Location = new System.Drawing.Point(138, 43);
+			this.labelSelectAct.Location = new System.Drawing.Point(48, 11);
 			this.labelSelectAct.Name = "labelSelectAct";
 			this.labelSelectAct.Size = new System.Drawing.Size(113, 15);
 			this.labelSelectAct.TabIndex = 0;
@@ -47,36 +47,39 @@ namespace SimpleDataApp
 			// 
 			// btnGoToAdd
 			// 
-			this.btnGoToAdd.Location = new System.Drawing.Point(117, 83);
+			this.btnGoToAdd.Location = new System.Drawing.Point(27, 51);
 			this.btnGoToAdd.Name = "btnGoToAdd";
 			this.btnGoToAdd.Size = new System.Drawing.Size(157, 23);
 			this.btnGoToAdd.TabIndex = 1;
 			this.btnGoToAdd.Text = "Добавить учетную запись";
 			this.btnGoToAdd.UseVisualStyleBackColor = true;
+			this.btnGoToAdd.Click += new System.EventHandler(this.btnGoToAdd_Click);
 			// 
 			// btnGoToFillOrCancel
 			// 
-			this.btnGoToFillOrCancel.Location = new System.Drawing.Point(102, 144);
+			this.btnGoToFillOrCancel.Location = new System.Drawing.Point(12, 112);
 			this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
 			this.btnGoToFillOrCancel.Size = new System.Drawing.Size(185, 23);
 			this.btnGoToFillOrCancel.TabIndex = 2;
 			this.btnGoToFillOrCancel.Text = "Заполнить или отменить заказ";
 			this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+			this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
 			// 
 			// btnExit
 			// 
-			this.btnExit.Location = new System.Drawing.Point(297, 226);
+			this.btnExit.Location = new System.Drawing.Point(66, 159);
 			this.btnExit.Name = "btnExit";
 			this.btnExit.Size = new System.Drawing.Size(75, 23);
 			this.btnExit.TabIndex = 3;
 			this.btnExit.Text = "Выйти";
 			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
 			// 
 			// Navigation
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(388, 265);
+			this.ClientSize = new System.Drawing.Size(207, 191);
 			this.Controls.Add(this.btnExit);
 			this.Controls.Add(this.btnGoToFillOrCancel);
 			this.Controls.Add(this.btnGoToAdd);
--- SimpleDataApp/Navigation.cs
+++ SimpleDataApp/Navigation.cs
@@ -17,5 +17,21 @@ namespace SimpleDataApp
 			InitializeComponent();
 		}
 
+		private void btnGoToAdd_Click(object sender, EventArgs e)
+		{
+			Form frm = new NewCustomer();
+			frm.Show();
+		}
+
+		private void btnGoToFillOrCancel_Click(object sender, EventArgs e)
+		{
+			Form frm = new FillOrCancel();
+			frm.ShowDialog();
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
 	}
 }
